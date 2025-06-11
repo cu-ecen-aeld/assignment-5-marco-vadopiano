ba67e43370bcc4dc44cc05c07052a41d0fca48d0
marco@vadopiano.it <marco@vadopiano.it>
2025-06-11 13:27:52 +0200

    assignment4-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,11 +5,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 3b3eadcd0fed6282864f3e09000f44b09e177b3c
+AESD_ASSIGNMENTS_VERSION = 63184ebacaab0e6b56765cc0ee3e78502777f062
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-lorenzo-castellani.git
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-marco-vadopiano.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
